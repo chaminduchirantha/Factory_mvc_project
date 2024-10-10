d258d1c3fa43bba94e1c5c084dd124b54855e463
chaminduchirantha <chaminduchirantha73@gmail.com>
2024-10-10 20:02:56 +0530

    Factory_mvc_Project

--- src/main/java/lk/ijse/gdse/factory_mvc_project/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/factory_mvc_project/AppInitializer.java
@@ -4,13 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class AppInitializer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("/View/WelcomePageView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("/View/LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 998,670);
         stage.setTitle("Garment Factory Management System");
         stage.setScene(scene);
