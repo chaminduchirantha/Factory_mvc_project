1dd173db577c435bf9470743beb20ac54005888e
chaminduchirantha <chaminduchirantha73@gmail.com>
2024-10-12 10:02:31 +0530

    Factory_mvc_Project

---
+++ main/java/lk/ijse/gdse/factory_mvc_project/dto/EmployeeDto.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse.factory_mvc_project.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class EmployeeDto {
+    private String employeeId;
+    private String employeeName;
+    private String employeeAge;
+    private String employeeAddress;
+    private String employeeSection;
+    private String employeeTask;
+    private String employeeContactNumber;
+}
