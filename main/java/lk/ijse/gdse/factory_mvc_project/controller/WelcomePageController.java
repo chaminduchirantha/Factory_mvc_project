aee70ae489c8e79055b55a41ca8f8d7558946cce
chaminduchirantha <chaminduchirantha73@gmail.com>
2024-11-21 19:17:10 +0530

    WelcomePage

--- main/java/lk/ijse/gdse/factory_mvc_project/controller/WelcomePageController.java
+++ main/java/lk/ijse/gdse/factory_mvc_project/controller/WelcomePageController.java
@@ -3,6 +3,7 @@ package lk.ijse.gdse.factory_mvc_project.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -24,9 +25,15 @@ public class WelcomePageController {
     private AnchorPane welcomeAnchorPane;
 
     @FXML
-    void loginOnAction(javafx.event.ActionEvent actionEvent) throws IOException {
-        welcomeAnchorPane.getChildren().clear();
-        AnchorPane load = FXMLLoader.load(getClass().getResource("/View/LoginView.fxml"));
-        welcomeAnchorPane.getChildren().add(load);
+    void loginOnAction(ActionEvent actionEvent){
+        try {
+            welcomeAnchorPane.getChildren().clear();
+            AnchorPane load = FXMLLoader.load(getClass().getResource("/View/LoginView.fxml"));
+            welcomeAnchorPane.getChildren().add(load);
+
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Welcome Page not found").show();
+
+        }
     }
 }
