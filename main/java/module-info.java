713c9e2855dd207a4bafda099f838e5d07dd9ecc
chaminduchirantha <chaminduchirantha73@gmail.com>
2024-11-21 19:20:04 +0530

    module info

--- main/java/module-info.java
+++ main/java/module-info.java
@@ -4,6 +4,8 @@ module lk.ijse.gdse.factory_mvc_project {
     requires java.desktop;
     requires lombok;
     requires java.sql;
+    requires net.sf.jasperreports.core;
+    requires java.mail;
 
     opens lk.ijse.gdse.factory_mvc_project.dto.tm to javafx.base;
     opens lk.ijse.gdse.factory_mvc_project.controller to javafx.fxml;
